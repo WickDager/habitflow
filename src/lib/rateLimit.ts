1e4464d2f2d447e9553e131888282453afc2f83f
WickDager <masariraa2006@gmail.com>
2026-05-22 19:26:51 +0300

    Mini App V2

--- src/lib/rateLimit.ts
+++ src/lib/rateLimit.ts
@@ -1,7 +1,18 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-export const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(30, "1 m"),
-});
+let ratelimitInstance: Ratelimit | null = null;
+
+function getRatelimit() {
+  if (!ratelimitInstance) {
+    ratelimitInstance = new Ratelimit({
+      redis: Redis.fromEnv(),
+      limiter: Ratelimit.slidingWindow(30, "1 m"),
+    });
+  }
+  return ratelimitInstance;
+}
+
+export const ratelimit = {
+  limit: (id: string) => getRatelimit().limit(id),
+};
